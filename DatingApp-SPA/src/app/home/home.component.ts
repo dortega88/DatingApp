b590af5ef5dc00d7cf5771c534df77e52fffabf0
Daniel Ortega <dortega88@gmail.com>
2020-02-12 14:45:53 -0600

    End of Lecture 48 Section 4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -9,24 +9,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5678/api/values').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
   }
 
   cancelRegisterMode(registerMode: boolean) {
