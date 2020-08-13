109e8ee7375695eff9245844c78009d0bc1221e7
Daniel J Ortega <dortega88@gmail.com>
2020-08-13 13:53:24 -0500

    Designing the Inbox and Outbox templates for SPA (#45)

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -30,12 +30,19 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.userService.getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage,
-      this.pagination.itemsPerPage, this.messageContainer)
-      .subscribe((res: PaginatedResult<Message[]>) => {
-        this.messages = res.result;
-        this.pagination = res.pagination;
-      }, error => {
+    this.userService
+    .getMessages(
+      this.authService.decodedToken.nameid,
+      this.pagination.currentPage,
+      this.pagination.itemsPerPage,
+      this.messageContainer
+    )
+      .subscribe(
+        (res: PaginatedResult<Message[]>) => {
+          this.messages = res.result;
+          this.pagination = res.pagination;
+      },
+      error => {
         this.alertify.error(error);
       });
   }
