7606ca7b842b5d3ee8050b930bfd225e92381ec6
Daniel Ortega <dortega88@gmail.com>
2020-08-25 11:07:56 -0500

    Adding the send message functionality and a challenge

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -12,6 +12,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() recipientId: number;
   messages: Message[];
+  newMessage: any = {};
 
   constructor(
     private userService: UserService,
@@ -30,6 +31,17 @@ export class MemberMessagesComponent implements OnInit {
       }, error => {
         this.alertify.error(error);
       });
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid, this.newMessage)
+      .subscribe((message: Message) =>{
+      this.messages.unshift(message);
+      this.newMessage.content = '';
+    }, error => {
+      this.alertify.error(error);
+    })
   }
 
 }
