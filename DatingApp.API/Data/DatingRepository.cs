c6ecbe8b4688c885aa8aa0eb20227fabde81b0f5
Daniel J Ortega <dortega88@gmail.com>
2020-09-09 11:30:31 -0500

    Published to Azure; updated files and added lazy loading (#62)

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -43,14 +43,13 @@ namespace DatingApp.API.Data {
         
         public async Task<User> GetUser(int id) 
         {
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(user => user.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
 
             return user;
         }
         public async Task<PagedList<User>> GetUsers(UserParams userParams) 
         {
-            var users = _context.Users.Include(p => p.Photos)
-                .OrderByDescending(u => u.LastActive).AsQueryable();
+            var users = _context.Users.OrderByDescending(u => u.LastActive).AsQueryable();
 
             users = users.Where(u => u.Id != userParams.UserId);
 
@@ -95,10 +94,7 @@ namespace DatingApp.API.Data {
 
         private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
         {
-            var user = await _context.Users
-                .Include(x => x.Likers)
-                .Include(x => x.Likees)
-                .FirstOrDefaultAsync(u => u.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
 
             if (likers)
             {
@@ -116,19 +112,12 @@ namespace DatingApp.API.Data {
 
         public async Task<Message> GetMessage(int id)
         {
-            var message = _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos);
-            
-            return await message.FirstOrDefaultAsync(m => m.Id == id);
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
         }
 
         public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
-            var messages = _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .AsQueryable();
+            var messages = _context.Messages.AsQueryable();
 
             switch (messageParams.MessageContainer)
             {
@@ -155,8 +144,6 @@ namespace DatingApp.API.Data {
         public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
             var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(m => m.RecipientId == userId && m.RecipientDeleted == false 
                     && m.SenderId == recipientId
                     || m.RecipientId == recipientId && m.SenderId == userId 
